be95ed6453de3f3046ae3ecb59b20a691f0f3dcf
Santiago Casañas Tabares 202025301 <santiago.casanas@correounivalle.edu.co>
2022-03-07 11:56:38 -0500

    Creacion jugadorPc

---
+++ src/myProject/JugadorPc.java
@@ -0,0 +1,38 @@
+package myProject;
+
+import java.util.Random;
+
+public class JugadorPc {
+    private int[][] barcosPc;
+    private boolean[][] disparosPc;
+
+
+    public JugadorPc() {
+        barcosPc = new int[10][10];
+        disparosPc = new boolean[10][10];
+    }
+
+
+    /**
+     * genera una coordenada aleatoria y marcaesa posicion como true para
+     * recordar que ahí ya hubo un disparo, en caso de haber un disparo previo
+     * realiza la instrucción de nuevo.
+     */
+    public void disparar(){
+        Random random = new Random();
+        int pcX, pcY;
+        do {
+            pcX = random.nextInt(10);
+            pcY = random.nextInt(10);
+            disparosPc[pcX][pcY] = true;
+        }while (disparosPc[pcX][pcY] == true);
+    }
+
+    public int[][] getBarcosPc() {
+        return barcosPc;
+    }
+
+    public boolean[][] getDisparosPc() {
+        return disparosPc;
+    }
+}
