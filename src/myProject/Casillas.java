0fa8b7d327fb5fc22f2bd663bfce24e86ec242d6
unknown <jesus.guetio@correounivalle.edu.co>
2022-03-17 19:06:06 -0500

    configurando escuchas

--- src/myProject/Casillas.java
+++ src/myProject/Casillas.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Casillas extends JButton {
+public class Casillas extends JLabel {
     private int idbarco, idImagen, posicion;
     private boolean disparo;
     private ImageIcon imagenAgua, imagenFallo, imagenTocado, imagenHundido;
